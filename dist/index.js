0692d75cffec23d2d6c3f216f6f7ad4e506578b0
Patt-Tom McDonnell <contact@mcdonnell.pt>
2017-09-07 13:24:41 +0100

    Initial Commit

---
+++ dist/index.js
@@ -0,0 +1,66 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Color = require("color");
+const math = require("mathjs");
+class NodeColorGradient {
+    constructor(colors) {
+        this.colors = colors.map((step) => {
+            return Color.rgb(step).hsv();
+        });
+        this.differences = this.calculateDifferences();
+    }
+    calculateDifferences() {
+        return Array.from(Array(this.colors.length - 1)).map((color, intervalIndex) => {
+            return this.colors[intervalIndex].color.map((val, index) => {
+                return this.colors[intervalIndex + 1].color[index] - val;
+            });
+        });
+    }
+    calculateIncrements(steps) {
+        const stopsPerColor = steps / (this.colors.length - 1);
+        const differenceMatrix = math.matrix(this.differences);
+        return math.divide(differenceMatrix, stopsPerColor).toArray();
+    }
+    calculateStopsForIndex(baseIndex, steps, totalSteps) {
+        if (!totalSteps) {
+            totalSteps = steps;
+        }
+        const baseMatrix = math.matrix(this.colors[baseIndex].color);
+        let stops = [];
+        const increments = this.calculateIncrements(totalSteps);
+        for (let i = 0; i < steps; i++) {
+            const multiplier = math.matrix([i + 1, i + 1, i + 1]);
+            const increment = math.matrix(increments[baseIndex]);
+            const a = baseMatrix;
+            const b = math.dotMultiply(multiplier, increment);
+            const stop = math.add(a, b);
+            stops.push(stop.toArray());
+        }
+        return stops;
+    }
+    getGradient(totalSteps) {
+        let stops = [];
+        const iterations = this.colors.length - 1;
+        const stopsPerColor = totalSteps / iterations;
+        for (let i = 0; i < iterations; i++) {
+            let newStops;
+            if (i === 0) {
+                newStops = this.calculateStopsForIndex(i, stopsPerColor, totalSteps);
+            }
+            else {
+                newStops = this.calculateStopsForIndex(i, stopsPerColor, totalSteps);
+                newStops.shift();
+            }
+            stops = stops.concat(newStops);
+        }
+        return stops.map((stop) => {
+            return Color.hsv(stop);
+        });
+    }
+    getStop(totalSteps, stop) {
+        return this.getGradient(totalSteps)[stop];
+    }
+}
+exports.default = NodeColorGradient;
+module.exports = NodeColorGradient;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import * as Color from 'color';
+import * as math from 'mathjs';
+
+export default class NodeColorGradient {
+
+  colors: Color[];
+  differences: number[];
+
+  constructor(colors: Array<number[]>) {
+    this.colors = colors.map((step: number[]) => {
+      return Color.rgb(step).hsv();
+    });
+
+    this.differences = this.calculateDifferences();
+  }
+
+  private calculateDifferences() {
+    return Array.from(Array(this.colors.length - 1)).map((color, intervalIndex) => {
+      return this.colors[intervalIndex].color.map((val, index) => {
+        return this.colors[intervalIndex + 1].color[index] - val;
+      });
+    });
+  }
+
+  private calculateIncrements(steps: number){
+    const stopsPerColor = steps / (this.colors.length - 1);
+    const differenceMatrix = math.matrix(this.differences);
+    return math.divide(differenceMatrix, stopsPerColor).toArray();
+  }
+
+  private calculateStopsForIndex(baseIndex: number, steps: number, totalSteps?: number){
+    if(!totalSteps){
+      totalSteps = steps;
+    }
+    const baseMatrix = math.matrix(this.colors[baseIndex].color);
+    let stops : Array<number[]> = [];
+    const increments = this.calculateIncrements(totalSteps);
+    for (let i = 0; i < steps; i++) {
+      const multiplier = math.matrix([i+1,i+1,i+1]);
+      const increment = math.matrix(increments[baseIndex]);
+      const a = baseMatrix;
+      const b = math.dotMultiply(multiplier, increment);
+      const stop = math.add(a,b);
+      stops.push(stop.toArray());
+    }
+    return stops;
+  }
+
+  public getGradient(totalSteps: number) : Array<Color> {
+    let stops : Array<number[]> = [];
+    const iterations = this.colors.length - 1;
+    const stopsPerColor = totalSteps / iterations;
+    for (let i = 0; i < iterations; i++) {
+      let newStops;
+      if(i === 0){
+        newStops = this.calculateStopsForIndex(i, stopsPerColor, totalSteps);
+      } else {
+        newStops = this.calculateStopsForIndex(i, stopsPerColor, totalSteps);
+        newStops.shift();
+      }
+      stops = stops.concat(newStops);
+    }
+    return stops.map((stop) => {
+      return Color.hsv(stop)
+    });
+  }
+
+  public getStop(totalSteps: number, stop: number) : Color {
+    return this.getGradient(totalSteps)[stop];
+  }
+
+}
+
+// Support require();
+module.exports = NodeColorGradient;
